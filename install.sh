0b86a6b898940706832dadbda67bbc906f576390
Jonas Götz <65551807+JonasGoetz01@users.noreply.github.com>
2023-12-08 19:52:36 +0100

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/JonasGoetz01/sui/main/masterplan.sh ~/masterplan.sh
+curl https://raw.githubusercontent.com/JonasGoetz01/sui/main/masterplan.sh > ~/masterplan.sh
 chmod +x ~/masterplan.sh
 echo "alias cd='bash ~/masterplan.sh'" >> ~/.zshrc
