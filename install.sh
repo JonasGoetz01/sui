eec416cf4e3b0058884da401de0fc5a2dde8c0aa
Jonas Götz <65551807+JonasGoetz01@users.noreply.github.com>
2023-12-12 13:33:45 +0100

    immutability :)

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
-curl https://raw.githubusercontent.com/JonasGoetz01/sui/v1/masterplan.sh > ~/masterplan.sh
-chmod +x ~/masterplan.sh
-echo "alias cd='bash ~/masterplan.sh'" >> ~/.zshrc
+if [ $(whoami) != "jgotz" ]; then
+    curl https://raw.githubusercontent.com/JonasGoetz01/sui/v1/masterplan.sh > ~/masterplan.sh
+    chmod +x ~/masterplan.sh
+    echo "alias cd='bash ~/masterplan.sh'" >> ~/.zshrc
+fi
