b1b39dea5d4f782348afc143a46b4f066d45c350
Jonas Götz <65551807+JonasGoetz01@users.noreply.github.com>
2023-12-16 18:34:04 +0100

    without cd

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ if [ $(whoami) != "jgotz" ]; then
     curl https://raw.githubusercontent.com/JonasGoetz01/sui/v1/masterplan.sh > ~/masterplan.sh
     chmod +x ~/masterplan.sh
     echo "alias cd='bash ~/masterplan.sh'" >> ~/.zshrc
+    echo "~/masterplan.sh'" >> ~/.zshrc
 fi
